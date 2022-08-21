d62c63f54b45be29594638256208f473ccfdf626
Alhassan Joshua Abel <saintjosh26@gmail.com>
2022-08-21 00:22:55 +0000

    Day 04 React Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import JoshuaImage from './Image/Joshua.jpg'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// Header Component
+const Header = () => (
+  <header>
+    <div className='header-wrapper'>
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Joshua Alhassan</p>
+      <small>Aug 20, 2022</small>
     </div>
-  );
+  </header>
+)
+
+// User Card Component
+const UserCard = () => (
+  <div className='user-card'>
+    <img src={JoshuaImage} alt='Joshua image' className='image' />
+    <h2>Joshua Alhassan</h2>
+  </div>
+)
+
+// TechList Component
+const TechList = () => {
+  const techs = ['HTML', 'CSS', 'JavaScript']
+  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+  return techsFormatted
 }
+
+// Main Component
+const Main = () => (
+  <main>
+    <div className='main-wrapper'>
+      <p>Prerequisite to get started react.js:</p>
+      <ul>
+        <TechList />
+      </ul>
+      <UserCard />
+    </div>
+  </main>
+)
+
+// Footer Component
+const Footer = () => (
+  <footer>
+    <div className='footer-wrapper'>
+      <p>Copyright 2020</p>
+    </div>
+  </footer>
+)
+
+// The App, or the parent or the container component
+const App = () => (
+  <div className='app'>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+)
+
+// const rootElement = document.getElementById('root')
+// // we render the App component using the ReactDOM package
+// ReactDOM.render(<App />, rootElement)
 
 export default App;
